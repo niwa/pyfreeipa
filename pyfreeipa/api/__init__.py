69c4c91cdbea96e86f5188fe5f2969fd4f6b3788
Aaron Hicks <aethylred@gmail.com>
2018-11-05 14:16:55 +1300

    Stub out the Api class

---
+++ pyfreeipa/api/__init__.py
@@ -0,0 +1,19 @@
+"""
+{ item_description }
+"""
+import requests
+import urllib
+import pathlib
+import json
+from typing import Union
+import urllib3
+import sys
+
+class Api(object):
+    """
+    @brief      Class for api.
+    """
+    def __init__(
+        self
+    ):
+        print("Does nothing")
