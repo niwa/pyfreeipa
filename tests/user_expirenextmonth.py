47298d8b7bee777bef69cc70a1f0306753638bfe
Aaron Hicks <aethylred@gmail.com>
2019-04-18 16:43:56 +1200

    Get the users with passwords that expire on the first of next month next year

---
+++ tests/user_expirenextmonth.py
@@ -0,0 +1,68 @@
+"""
+A generic wrapper script for the pyfreeipa Api class
+"""
+import json
+import sys
+import datetime
+from pyfreeipa.Api import Api
+from pyfreeipa.configuration import CONFIG
+
+
+def main():
+    """
+    @brief      This provides a wrapper for the pyfreeipa module
+
+    @return     { description_of_the_return_value }
+    """
+
+    if CONFIG['command'] == 'dumpconfig':
+        print(json.dumps(CONFIG, indent=2, sort_keys=True))
+        sys.exit(0)
+
+    # Define API session
+    ipaapi = Api(
+        host=CONFIG['ipaserver']['host'],
+        username=CONFIG['ipaserver']['user'],
+        password=CONFIG['ipaserver']['password'],
+        port=CONFIG['ipaserver']['port'],
+        verify_ssl=CONFIG['ipaserver']['verify_ssl'],
+        verify_method=CONFIG['ipaserver']['verify_method'],
+        verify_warnings=CONFIG['ipaserver']['verify_warnings'],
+        dryrun=CONFIG['dryrun']
+    )
+
+    if CONFIG['users'] and CONFIG['groups']:
+        users = ipaapi.users(
+            uid=CONFIG['users'],
+            in_group=CONFIG['groups']
+        )
+    elif CONFIG['users']:
+        users = ipaapi.users(CONFIG['users'])
+    elif CONFIG['groups']:
+        users = ipaapi.users(in_group=CONFIG['groups'])
+    else:
+        users = ipaapi.users()
+
+    today = datetime.date.today()
+    nextmonth = today.replace(month=(today.month + 1), day=1, year=(today.year + 1))
+    expiringusers = []
+
+    for user in users:
+        if 'krbpasswordexpiration' in user:
+            expiry = user['krbpasswordexpiration']
+            if isinstance(expiry, datetime.datetime):
+                if today <= expiry.date() <= nextmonth:
+                    expiringusers.append(user['uid'])
+
+    print("Users with passwords expiring between %s and %s: %s" %
+        (
+            str(today),
+            str(nextmonth),
+            expiringusers
+        )
+    )
+    print("Total expiring users: %s" % len(expiringusers))
+
+
+if __name__ == "__main__":
+    main()
