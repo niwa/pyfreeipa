f2a1c7b141f753e65d77a56df04199cb57462678
Aaron Hicks <aaron.hicks@niwa.co.nz>
2025-05-30 17:10:09 +1200

    Create user_add method

--- pyfreeipa/Api/_user/__init__.py
+++ pyfreeipa/Api/_user/__init__.py
@@ -536,3 +536,244 @@ def user_mod(
         response = prepared
 
     return response
+
+
+def user_add(
+        self,
+        # Arguments
+        uid: type=str,
+        # Options
+        givenname: Union[str, None]=None,
+        sn: Union[str, None]=None,
+        cn: Union[str, None]=None,
+        displayname: Union[str, None]=None,
+        initials: Union[str, None]=None,
+        homedirectory: Union[str, None]=None,
+        gecos: Union[str, None]=None,
+        loginshell: Union[str, None]=None,
+        krbprincipalname: Union[str, None]=None,
+        krbprincipalexpiration: Union[datetime, None]=None,
+        krbpasswordexpiration: Union[datetime, None]=None,
+        mail: Union[str, None]=None,
+        userpassword: Union[str, None]=None,
+        random: Union[bool, None]=None,
+        uidnumber: Union[int, None]=None,
+        gidnumber: Union[int, None]=None,
+        telephonenumber: Union[str, None]=None,
+        mobile: Union[str, None]=None,
+        ou: Union[str, None]=None,
+        title: Union[str, None]=None,
+        manager: Union[str, None]=None,
+        carlicense: Union[str, None]=None,
+        ipasshpubkey: Union[str, None]=None,
+        ipauserauthtype: Union[str, None]=None,
+        userclass: Union[str, None]=None,
+        ipatokenradiusconfiglink: Union[str, None]=None,
+        ipatokenradiususername: Union[str, None]=None,
+        departmentnumber: Union[str, None]=None,
+        employeenumber: Union[str, None]=None,
+        employeetype: Union[str, None]=None,
+        preferredlanguage: Union[str, None]=None,
+        nsaccountlock: Union[str, None]=None,
+        no_members: Union[bool, None]=None,
+        rename: Union[str, None]=None,
+        allattrs: Union[bool, None]=None,
+        raw: Union[bool, None]=None,
+        addattr: Union[dict, None]=None,
+        setattr: Union[dict, None]=None,
+        delattr: Union[dict, None]=None
+):
+
+    method = 'user_add'
+
+    args = uid
+
+    params = {}
+
+    # Case insensitive!
+    if givenname is not None:
+        params['givenname'] = givenname
+
+    if sn is not None:
+        params['sn'] = sn
+
+    # Case insensitive!
+    if cn is not None:
+        params['cn'] = cn
+
+    # Case insensitive!
+    if displayname is not None:
+        params['displayname'] = displayname
+
+    # Case insensitive! Should be .upper
+    if initials is not None:
+        params['initials'] = initials
+
+    # Case insensitive!
+    if homedirectory is not None:
+        params['homedirectory'] = homedirectory
+
+    # Case insensitive!
+    if gecos is not None:
+        params['gecos'] = gecos
+
+    if loginshell is not None:
+        params['loginshell'] = loginshell
+
+    if krbprincipalname is not None:
+        params['krbprincipalname'] = krbprincipalname
+
+    if krbprincipalexpiration is not None:
+        params['krbprincipalexpiration'] = krbprincipalexpiration.strftime("%Y%m%d%H%M%SZ")
+
+    if krbpasswordexpiration is not None:
+        params['krbpasswordexpiration'] = krbpasswordexpiration.strftime("%Y%m%d%H%M%SZ")
+
+    # Case insensitive!
+    if mail is not None:
+        params['mail'] = mail
+
+    if userpassword is not None:
+        params['userpassword'] = userpassword
+
+    if random is not None:
+        params['random'] = random
+
+    if uidnumber is not None:
+        params['uidnumber'] = uidnumber
+
+    if gidnumber is not None:
+        params['gidnumber'] = gidnumber
+
+    if telephonenumber is not None:
+        params['telephonenumber'] = telephonenumber
+
+    if mobile is not None:
+        params['mobile'] = mobile
+
+    if ou is not None:
+        params['ou'] = ou
+
+    # Case insensitive!
+    if title is not None:
+        params['title'] = title
+
+    # Case insensitive!
+    if manager is not None:
+        params['manager'] = manager
+
+    if carlicense is not None:
+        params['carlicense'] = carlicense
+
+    if ipasshpubkey is not None:
+        params['ipasshpubkey'] = ipasshpubkey
+
+    if ipauserauthtype is not None:
+        params['ipauserauthtype'] = ipauserauthtype
+
+    if userclass is not None:
+        params['userclass'] = userclass
+
+    if ipatokenradiusconfiglink is not None:
+        params['ipatokenradiusconfiglink'] = ipatokenradiusconfiglink
+
+    if ipatokenradiususername is not None:
+        params['ipatokenradiususername'] = ipatokenradiususername
+
+    # Case insensitive!
+    if departmentnumber is not None:
+        params['departmentnumber'] = departmentnumber
+
+    if employeenumber is not None:
+        params['employeenumber'] = employeenumber
+
+    # Case insensitive!
+    if employeetype is not None:
+        params['employeetype'] = employeetype
+
+    # Case insensitive!
+    if preferredlanguage is not None:
+        params['preferredlanguage'] = preferredlanguage
+
+    if nsaccountlock is not None:
+        params['nsaccountlock'] = nsaccountlock
+
+    if no_members is not None:
+        params['no_members'] = no_members
+
+    if rename is not None:
+        params['rename'] = rename
+
+    if allattrs is not None:
+        params['all'] = allattrs
+
+    if raw is not None:
+        params['raw'] = raw
+
+    # Use addattr, setattr and delatter to modify custom variables
+    if setattr is not None:
+        for key, value in setattr.items():
+            if params.get('setattr'):
+                params['setattr'].append(
+                    "%s=%s" % (
+                        key,
+                        value
+                    )
+                )
+            else:
+                params['setattr']=[
+                    "%s=%s" % (
+                        key,
+                        value
+                    )
+                ]
+
+    if addattr is not None:
+        for key, value in addattr.items():
+            if params.get('addattr'):
+                params['addattr'].append(
+                    "%s=%s" % (
+                        key,
+                        value
+                    )
+                )
+            else:
+                params['addattr']=[
+                    "%s=%s" % (
+                        key,
+                        value
+                    )
+                ]
+
+    if delattr is not None:
+        for key, value in delattr.items():
+            if params.get('delattr'):
+                params['delattr'].append(
+                    "%s=%s" % (
+                        key,
+                        value
+                    )
+                )
+            else:
+                params['delattr']=[
+                    "%s=%s" % (
+                        key,
+                        value
+                    )
+                ]
+
+
+    # This is a write method so, prepare the request
+    prepared = self.preprequest(
+        method,
+        args=args,
+        params=params
+    )
+
+    # then check if it's a dryrun before executing it
+    if not self._dryrun:
+        response = self._session.send(prepared)
+    else:
+        response = prepared
+
+    return response
